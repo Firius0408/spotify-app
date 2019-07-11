bba648c2b598f89aea71775a8b03de5d611929d8
Brian Cheng <brian.cheng@ucla.edu>
2019-07-11 22:44:55 +0000

    Add comments to update.py

--- update.py
+++ update.py
@@ -10,6 +10,7 @@ client_secret = '160f08d795334c85a7227b695b1d54f3'
 refreshtokenme = 'AQBzPXpkLqDOctHGd092exRtHaw0WIg7wtgNB9BFKlJtMJboyq_EFHruvhUbLKp38AByxmss_kN1ViTwaroT0T_QbpOuEV9tn4O8AvXxsawTHnrap8pnwG98u3qYw1j8rS8'
 useridme = 'ohjber0jdol59842ilmd8w0a2'
 
+# returns a valid access token for the bot
 def accessTokenBot():
     url = 'https://accounts.spotify.com/api/token'
     headers = { 'Authorization': 'Basic ' + base64.b64encode(client_id + ':' + client_secret) }
@@ -21,6 +22,7 @@ def accessTokenBot():
     if (r.status_code == 200):
         return r.json()['access_token']
 
+# returns a valid access token for the user object
 def accessTokenForUser(user):
     refresh_token = user['refresh_token']
     url = 'https://accounts.spotify.com/api/token'
@@ -33,6 +35,7 @@ def accessTokenForUser(user):
     if (r.status_code == 200):
         return r.json()['access_token']
 
+# updates the three continuously updated playlists for the given user object
 def updateIndividual(user, botToken):
     playlisthreflong = user['playlisthreflong']
     playlisthrefmid = user['playlisthrefmid']
@@ -54,6 +57,8 @@ def updateIndividual(user, botToken):
     z.join()
     print('finished updating short playlist for user ' + user['id'])
 
+# Populates the given playlist with the current top songs for the given term for the user with accessTokenForUser
+# accessTokenPlaylist for access token of owner of given playlist in playlisthref
 def updatePlaylist(accessTokenUser, accessTokenPlaylist, term, playlisthref):
     url = 'https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=' + term
     headers = {'Authorization': 'Bearer ' + accessTokenUser }
@@ -69,6 +74,7 @@ def updatePlaylist(accessTokenUser, accessTokenPlaylist, term, playlisthref):
             }
     r = requests.put(url, headers=headers, data=json.dumps({'uris': uri}))
 
+# updates all continuously updated playlists for all users in users.json
 def update():
     print('update initiated at ' + date.strftime("%Y-%m-%d %H:%M:%S"))
     print('\n\n\n')
